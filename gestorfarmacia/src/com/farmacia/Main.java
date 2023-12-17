11e708a160e0a9894c9b5d8548e92e49efd97e97
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-12-17 20:21:24 +0000

    controller

--- gestorfarmacia/src/com/farmacia/Main.java
+++ gestorfarmacia/src/com/farmacia/Main.java
@@ -4,6 +4,7 @@ import com.farmacia.controller.MenuController;
 
 public class Main {
     public static void main(String[] args) {
-        MenuController.exibirMenuPrincipal();
+        MenuController menuController = new MenuController();
+        menuController.exibirMenuPrincipal();
     }
 }
--- gestorfarmacia/src/com/farmacia/controller/MenuController.java
+++ gestorfarmacia/src/com/farmacia/controller/MenuController.java
@@ -1,7 +1,11 @@
 package com.farmacia.controller;
 
+import java.util.Scanner;
+
 public class MenuController {
-    public static void exibirMenuPrincipal(){
-    
+    public static void exibirMenuPrincipal() {
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
     }
 }
