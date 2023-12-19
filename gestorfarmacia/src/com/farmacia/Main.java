de26714f2fde42ab2ed1e7f914c46eb60df1afe5
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-12-19 16:29:41 +0000

    main java meidcamento

--- gestorfarmacia/src/com/farmacia/Main.java
+++ gestorfarmacia/src/com/farmacia/Main.java
@@ -1,10 +1,16 @@
 package com.farmacia;
 
 import com.farmacia.controller.MenuController;
+import com.farmacia.model.Medicamento;
 
 public class Main {
     public static void main(String[] args) {
         MenuController menuController = new MenuController();
         menuController.exibirMenuPrincipal();
     }
+    public class void main (String[]) args{
+        Medicamento medicamento = new Medicamento();
+        Medicamento.exibirMenuPrincipal();
+    }
 }
+
