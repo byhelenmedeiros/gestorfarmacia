800c515593ad98e3dbf9de76b627ac42961d4e15
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-12-21 20:54:18 +0000

    organizar classes

--- gestorfarmacia/src/com/farmacia/model/Medicamento.java
+++ gestorfarmacia/src/com/farmacia/model/Medicamento.java
@@ -1,10 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-
+import java.util.Scanner;
 
 public class Medicamento {
     private String nome;
@@ -23,101 +19,55 @@ public class Medicamento {
         this.tipo = tipo;
     }
 
-public class GerirMedicamentos {
-    private List<Medicamento> listaMedicamentos;
-    private Scanner scanner;
-    private DateTimeFormatter formatter;
-
-    public GerirMedicamentos() {
-        this.listaMedicamentos = new ArrayList<>();
-        this.scanner = new Scanner(System.in);
-        this.formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    public String getNome() {
+        return nome;
     }
 
-    public void cadastroMedicamento() {
-        boolean continuarCadastro = true;
-
-        while (continuarCadastro) {
-            System.out.println("------Cadastro de novos medicamentos------");
-            System.out.println("Nome do Medicamento: ");
-                String nome = scanner.nextLine();
-            System.out.println("Quantidade: ");
-                int quantidade = Integer.parseInt(scanner.nextLine());
-            System.out.println("Data de Validade (dd/MM/yyyy): ");
-                String dataValidadeStr = scanner.nextLine();
-                    LocalDate dataValidade = LocalDate.parse(dataValidadeStr, formatter);
-            System.out.println("Descrição do medicamento: ");
-                String descricao = scanner.nextLine();
-            System.out.println("Preço: ");
-                double preco = Double.parseDouble(scanner.nextLine());
-            System.out.println("Tipo de medicamento: ");
-                String tipo = scanner.nextLine();
-                
-
-            Medicamento novoMedicamento = new Medicamento(nome, quantidade, dataValidade, descricao, preco, tipo);
-            listaMedicamentos.add(novoMedicamento);
-
-            System.out.println("Medicamento cadastrado com sucesso! Pretende cadastrar novo medicamento ?");
-            System.out.println("1 - Não");
-            System.out.println("2 - Sim, continuar cadastro de medicamentos");
-            System.out.println("3 - Voltar para menu principal");
-
-            int escolha = Integer.parseInt(scanner.nextLine());
-
-            switch (escolha) {
-                case 1:
-                    continuarCadastro = false;
-                    break;
-                case 2:
-                    continuarCadastro = true;
-                    break;
-                case 3:
-                    mostrarMenuPrincipal();
-                    break;
-                default:
-                    System.out.println("Escolha inválida, encerrando cadastro de medicamentos!");
-                    continuarCadastro = false;
-                    break;
-            }
-        }
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    public void vender(Medicamento medicamento, int quantidadeAVender) {
-        if (quantidadeAVender <= medicamento.getQuantidade() && quantidadeAVender > 0) {
-            medicamento.setQuantidade(medicamento.getQuantidade() - quantidadeAVender);
-            System.out.println(quantidadeAVender + " unidades de " + medicamento.getNome() + " vendidas.");
-        } else {
-            System.out.println("Quantidade inválida para venda.");
-        }
+    public int getQuantidade() {
+        return quantidade;
     }
 
-    private void mostrarMenuPrincipal() {
-       
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
 
-    public void mostrarTodosMedicamentos() {
-        if (listaMedicamentos.isEmpty()) {
-            System.out.println("Não há medicamentos cadastrados.");
-        } else {
-            System.out.println("Detalhes de todos os medicamentos:");
-            for (Medicamento medicamento : listaMedicamentos) {
-                System.out.println("Nome do medicamento: " + medicamento.getNome());
-                System.out.println("Quantidade em estoque: " + medicamento.getQuantidade());
-                System.out.println("Data de validade: " + medicamento.getDataValidade().format(formatter));
-                System.out.println("Preço: " + medicamento.getPreco());
-                System.out.println("Descrição do medicamento: " + medicamento.getDescricao());
-                System.out.println("Tipo de medicamento: " + medicamento.getTipo());
-                System.out.println("---------------");
-            }
-        }
+    public LocalDate getDataValidade() {
+        return dataValidade;
     }
-     private LocalDate dataValidade;
 
-        public Medicamento(LocalDate dataValidade) {
-            this.dataValidade = dataValidade;
-        }
+    public void setDataValidade(LocalDate dataValidade) {
+        this.dataValidade = dataValidade;
+    }
 
-           public void validarDataDeValidade() {
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void validarDataDeValidade() {
         LocalDate hoje = LocalDate.now();
         long diasAteVencimento = ChronoUnit.DAYS.between(hoje, dataValidade);
 
@@ -156,8 +106,5 @@ public class GerirMedicamentos {
         } else {
             System.out.println("A data de validade do medicamento está OK.");
         }
-    }
-
-
     }
 }
