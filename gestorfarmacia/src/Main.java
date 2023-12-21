d17942915e47610b716ad05599510074c2e3fa14
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-12-21 22:40:27 +0000

    instaciar a gerirmendicamentos

--- gestorfarmacia/src/Main.java
+++ gestorfarmacia/src/Main.java
@@ -1,10 +1,12 @@
 
-
 import com.farmacia.controller.MenuController;
 
 public class Main {
     public static void main(String[] args) {
         MenuController menuController = new MenuController();
         menuController.exibirMenuPrincipal();
+
+        GerirMedicamentos gerirMedicamentos = new GerirMedicamentos();
+        gerirMedicamentos.cadastroMedicamento();
     }
 }
